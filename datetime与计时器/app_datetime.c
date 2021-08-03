0ca023d3860924a8d5d18f2ba62ecf49b1f7dc00
liuhao1946 <2401569420@qq.com>
2021-08-03 22:43:10 +0800

    datetime与计时器

---
+++ datetime与计时器/app_datetime.c
@@ -0,0 +1,437 @@
+#include "app_datetime.h"
+
+
+static sec_chr_t sec_chr;
+static uint32_t  sec_chr_rtc_pre;
+static datetime_t datetime;
+
+/////////////////////////////////////////////////////////////////////////
+
+void app_rtc_cnt_diff(uint32_t *pdiff)
+{
+  static uint32_t  time_old = 0;
+  uint32_t time_x,time_new;
+	
+  time_new = RTC_GET_CNT();
+		 
+  if(time_new >= time_old)
+  {
+	  time_x = time_new - time_old;
+  }
+  else
+  {
+	  time_x = RTC_CNT_REG_MAX - time_old + time_new;
+  }
+	 
+  time_old = time_new;
+	 
+  *pdiff = time_x;
+}
+
+void app_datetime_updata(uint16_t sec)
+{
+  uint8_t temp;
+  uint16_t year;
+
+  datetime.seconds += sec;	
+  if(datetime.seconds > 59)
+  {
+    temp = datetime.seconds / 60;
+    datetime.seconds = datetime.seconds - temp * 60;
+    datetime.minutes += temp;	
+  }
+  if(datetime.minutes > 59)
+  {
+	datetime.minutes = 0;
+    datetime.hour++;	
+  }
+  if(datetime.hour > 23)
+  {
+ 	datetime.hour = 0;
+	datetime.day++;
+  }
+  
+  switch(datetime.month)
+  {
+	 case 2:
+        year = datetime.year + 2000;
+	    if((year % 400) == 0 || (year % 4) == 0)
+		{
+		  if(datetime.day > 29)
+		  {
+			  datetime.day = 1;
+			  datetime.month++;
+		  }
+		}
+		else
+	    {
+		  if(datetime.day > 28)
+		  {
+			  datetime.day = 1;
+			  datetime.month++;
+		  }
+		}
+	  break;
+				
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		if(datetime.day > 30)
+		{
+		    datetime.day = 1;	
+		    datetime.month++;
+		}
+	  break;
+		
+	default:
+		if(datetime.day > 31)
+		{
+		    datetime.day = 1;
+            datetime.month++;		  
+		}
+		break;
+  }
+	
+  if(datetime.month > 12)
+  {
+	  datetime.month = 1; 
+	  datetime.year++;
+  }
+  if(datetime.year > 100)
+  {
+	  datetime.year = 99; 
+  }
+}
+
+void app_datetime_get_day_before_date(datetime_t *datetime,datetime_t *dt_pre)
+{
+	if (datetime->month == 3 && datetime->day == 1)
+	{
+		dt_pre->year = datetime->year;
+		dt_pre->month = 2;
+		if ((datetime->year % 400) == 0 || (datetime->year % 4) == 0)
+		{
+			dt_pre->day = 29;
+		}
+		else
+		{
+			dt_pre->day = 28;
+		}
+	}
+	else if (datetime->month == 1 && datetime->day == 1)
+	{
+		dt_pre->year = datetime->year - 1;
+		dt_pre->month = 12;
+		dt_pre->day = 31;
+	}
+	else if (datetime->day == 1)
+	{
+		dt_pre->month = datetime->month - 1;
+		dt_pre->year = datetime->year;
+		
+		if(dt_pre->month == 1 || dt_pre->month == 3 || dt_pre->month == 5 || \
+			dt_pre->month == 7 || dt_pre->month == 8 || dt_pre->month == 10 || \
+		  dt_pre->month == 12)
+		{
+			dt_pre->day = 31;
+		}
+		else
+		{
+			dt_pre->day = 30;
+		}
+	}
+	else
+	{
+		dt_pre->day = datetime->day - 1;
+		dt_pre->month = datetime->month;
+		dt_pre->year = datetime->year;
+	}
+
+	dt_pre->hour = datetime->hour;
+	dt_pre->minutes = datetime->minutes;
+	dt_pre->seconds = datetime->seconds;
+}
+
+//time_min <= 24*60(min)
+void app_datetime_get_pre_date_time(datetime_t *dt_cur, datetime_t *p,uint16_t time_min)
+{
+	uint16_t total_min;
+	uint16_t delta;
+    datetime_t dt_pre;
+	
+	total_min = dt_cur->hour*60 + dt_cur->minutes;
+	if(total_min >= time_min)
+	{
+		delta = total_min - time_min;
+		p->hour = delta / 60;
+		p->minutes = delta % 60;
+		p->day = dt_cur->day;
+		p->month = dt_cur->month;
+		p->year = dt_cur->year;
+	}
+	else
+	{
+	  app_datetime_get_day_before_date(dt_cur,&dt_pre);
+		
+		delta = time_min - total_min;
+		delta = (uint16_t)24 * 60 - delta;
+		
+		p->hour = delta / 60;
+		p->minutes = delta % 60;
+		p->day = dt_pre.day;
+		p->month = dt_pre.month;
+		p->year = dt_pre.year;
+	}
+	
+	p->seconds = dt_cur->seconds;
+}
+
+//time_min <= 24*60(min)
+void app_datetime_get_behind_date_time(datetime_t *dt_cur, datetime_t *p,uint16_t time_min)
+{
+    uint16_t total_min;
+	uint8_t h,m;
+	
+	total_min = dt_cur->hour*60 + dt_cur->minutes;
+	total_min += time_min;
+	
+	h = total_min / 60;
+	m = total_min % 60;
+	
+	p->hour = dt_cur->hour;
+	p->minutes = dt_cur->minutes;
+	p->day = dt_cur->day;
+	p->month = dt_cur->month;
+	p->year = dt_cur->year;
+	p->seconds = dt_cur->seconds;
+
+	if(h >= 24)
+	{
+		h = 23;
+		m = 59;
+	}
+	p->hour = h;
+	p->minutes = m;
+}
+
+void app_datetime_synch(void )
+{
+    uint32_t diff;
+    uint32_t sec_cnt;
+    static uint32_t total_diff = 0; 
+  
+    app_rtc_cnt_diff(&diff);
+  
+    total_diff += diff;
+    sec_cnt = total_diff / RTC_FREQUEN;
+  
+	if(sec_cnt != 0)
+	{
+	  total_diff = total_diff - sec_cnt*RTC_FREQUEN;
+	}
+	
+  app_datetime_updata(sec_cnt);
+}
+
+void app_datetime_init(void )
+{
+  uint32_t diff;
+	
+  datetime.seconds = 00;
+  datetime.minutes = 00;
+  datetime.hour = 2;
+  datetime.day = 30;
+  datetime.month = 12;
+  datetime.year = 21;
+	
+  app_rtc_cnt_diff(&diff);
+}
+
+void app_datetime_set(datetime_t *dt)
+{
+  datetime.seconds = dt->seconds;
+  datetime.minutes = dt->minutes;
+  datetime.hour = dt->hour;
+  datetime.day = dt->day;
+  datetime.month = dt->month;
+  datetime.year = dt->year;
+}
+
+void app_datetime_get(datetime_t *dt)
+{
+  dt->seconds = datetime.seconds;
+  dt->minutes = datetime.minutes;
+  dt->hour = datetime.hour;
+  dt->day = datetime.day;
+  dt->month = datetime.month;
+  dt->year = datetime.year;
+}
+
+int app_datetime_week_cal(datetime_t *dt)
+{
+	int week = 0;
+	int y, m, d;
+	
+	y = dt->year;
+	m = dt->month;
+	d = dt->day;
+		
+    if(m==1||m==2) 
+	{
+        m+=12;
+        y--;
+    }
+	 
+    week=(d+2*m+3*(m+1)/5+y+y/4-y/100+y/400)%7;
+ 
+	return week;
+}
+
+void app_datetime_get_specify_date_week(datetime_t *dt, uint8_t *pwk)
+{
+	*pwk = app_datetime_week_cal(dt);
+}
+
+void app_datetime_UTC_to_beijing(datetime_t *utc_time, datetime_t *bj_time, int16_t time_zone)
+{
+	uint8_t days = 0;
+
+    bj_time->day = utc_time->day
+    bj_time->month = utc_time->month;
+
+	
+	if (time->month == 1 || time->month == 3 || time->month == 5 || \
+		  time->month == 7 || time->month == 8 || time->month == 10 || time->month == 12)
+	{
+		days = 31;
+	}
+	else if (time->month == 4 || time->month == 6 || time->month == 9 || time->month == 11)
+	{
+		days = 30;
+	}
+	else if (time->month == 2)
+	{
+		if ((time->year % 400 == 0) || ((time->year % 4 == 0) && (time->year % 100 != 0))) 
+		{
+			days = 29;
+		}
+		else
+		{
+			days = 28;
+		}
+	}
+	
+	time->hour += time_zone;
+	if (time->hour >= 24)         
+	{
+		time->hour -= 24;
+		time->day++;
+		if (time->day > days)     
+		{
+			time->day = 1;
+			time->month++;
+			if (time->month > 12)  
+			{
+				time->year++;
+			}
+		}
+	}
+}
+
+void app_datetime_get_week(uint8_t *pwk)
+{
+  *pwk = app_datetime_week_cal(&datetime);
+}
+
+////////////////////////////////////////////////
+//second chronograph
+static uint32_t sec_chr_total_diff = 0; 
+uint32_t app_datetime_sec_chr_get_rtc_diff(void )
+{
+	uint32_t rtc_cur,rtc_x;
+
+	rtc_cur = app_get_cur_rtc_cnt();
+	if(rtc_cur >= sec_chr_rtc_pre)
+    {
+	  rtc_x = rtc_cur - sec_chr_rtc_pre;
+    }
+    else
+    {
+	  rtc_x = RTC_CNT_REG_MAX - sec_chr_rtc_pre + rtc_cur;
+    }
+	 
+    sec_chr_rtc_pre = rtc_cur;
+	
+	return rtc_x;
+}
+
+void app_datetime_syn_rtc(void )
+{
+	sec_chr_rtc_pre = app_get_cur_rtc_cnt();
+}
+
+void app_datetime_sec_chr_init(void )
+{
+	sec_chr.sec = 0;
+	sec_chr.ms = 0;
+	sec_chr.hour = 0;
+	sec_chr.min = 0;
+	sec_chr_total_diff = 0;
+	sec_chr_rtc_pre = app_get_cur_rtc_cnt();
+}
+
+void app_datetime_get_sec_chr(sec_chr_t *psec)
+{
+	psec->hour = sec_chr.hour;
+	psec->min = sec_chr.min;
+	psec->ms = sec_chr.ms;
+	psec->sec = sec_chr.sec;
+}
+
+void app_datetime_set_sec_chr(sec_chr_t *psec)
+{
+	sec_chr.hour = psec->hour;
+	sec_chr.min = psec->min;
+	sec_chr.sec = psec->sec;
+	sec_chr.ms = psec->ms;
+}
+
+void app_datetime_sec_chr_syn(void )
+{
+	uint32_t rtc_diff;
+	uint8_t time_ms;
+	 
+	rtc_diff = app_datetime_sec_chr_get_rtc_diff();
+	sec_chr_total_diff += rtc_diff;
+	time_ms = sec_chr_total_diff / (RTC_FREQUEN / 10);
+	
+	if(time_ms != 0)
+	{
+		sec_chr_total_diff = sec_chr_total_diff - time_ms * (RTC_FREQUEN / 10);
+	}
+	
+	sec_chr.ms += time_ms;
+	if(sec_chr.ms > 9)
+	{
+		uint8_t temp;
+		
+		temp = sec_chr.ms / 10;
+	  	sec_chr.ms = sec_chr.ms - temp * 10;
+    	sec_chr.sec += temp;	
+	}
+	if(sec_chr.sec > 59)
+	{
+		sec_chr.sec = 0;
+		sec_chr.min++;
+	}
+	if(sec_chr.min > 59)
+	{
+		sec_chr.min = 0;
+		sec_chr.hour++;
+	}
+	if(sec_chr.hour > SEC_CHR_HOUR_MAX)
+	{
+		sec_chr.hour = 99;
+	}
+}
---
+++ datetime与计时器/app_datetime.h
@@ -0,0 +1,211 @@
+#ifndef _APP_DATETIME_
+#define _APP_DATETIME_
+
+
+#include "stdint.h"
+
+//RTC使用的低频晶振频率，一般是32000或者32768
+#define RTC_FREQUEN       32768
+//RTC寄存器的最大值
+#define RTC_CNT_REG_MAX   0x00ffffff
+/*
+获得RTC的计数值，这个接口需要应用层实现：
+
+uint32_t app_get_cur_rtc_cnt(void )
+{
+   return RTC计数器;
+}
+*/
+extern uint32_t app_get_cur_rtc_cnt(void );
+
+#define RTC_GET_CNT() app_get_cur_rtc_cnt() 
+
+//////////////////////////////////////////
+
+typedef struct
+{
+  uint8_t seconds;
+  uint8_t minutes;
+  uint8_t hour;
+  uint8_t day;
+  uint8_t month;
+  uint8_t year;
+}datetime_t;
+
+
+//datetime模块初始化
+void app_datetime_init(void );
+
+//datetime设置
+void app_datetime_set(datetime_t *dt);
+
+//获得当前datetime
+void app_datetime_get(datetime_t *dt);
+
+/*
+获得当天对应的星期，结果保存在pwk中
+
+注意：
+0 - 星期一
+6 - 星期天
+*/
+void app_datetime_get_week(uint8_t *pwk);
+
+//获得指定日期对应的星期
+void app_datetime_get_specify_date_week(datetime_t *dt, uint8_t *pwk);
+
+//把UTC时间转化为北京时间，北京时区为8
+void app_datetime_UTC_to_beijing(datetime_t *utc_time, datetime_t *bj_time, int16_t time_zone);
+
+/*
+时钟同步函数。这个函数需要间隔调用，调用间隔最大值由RTC所用的晶振决定
+
+以32768Hz为例，RTC计数器每隔1/32768(大约等于30.5us)累加一次。由于RTC
+计数周期为0xffffff，因此RTC的溢出周期是8.5分钟。只要在溢出周期到达前调
+用该接口同步一次RTC，就可以确保时间的准确性。
+
+因此，在外部晶振使用32768时，app_datetime_synch()的调用间隔<8.5min。
+
+一般而言，如果你需要显示时钟的秒，那么这个函数每隔1s调用一次是合适的，
+如果不需要显示s，每隔1分钟之内调用一次是合适的
+*/
+void app_datetime_synch(void );
+
+//获得datetime指定的日期前一天的时间并保存在dt_pre中
+void app_datetime_get_day_before_date(datetime_t *datetime,datetime_t *dt_pre);
+
+//获得dt_cur指定日期前time_min分钟的时间并保存在p中。注意，time_min <= 24*60(分钟)
+void app_datetime_get_pre_date_time(datetime_t *dt_cur,datetime_t *p,uint16_t time_min);
+
+//获得dt_cur指定日期后time_min分钟的时间并保存在p中。注意，time_min < 24*60(分钟)
+void app_datetime_get_behind_date_time(datetime_t *dt_cur, datetime_t *p,uint16_t time_min);
+
+
+/*
+例子：
+1、在程序开始时，初始化datetime
+app_datetime_init()
+
+2、设置当前时间为2021.8.1/00:00:00
+datetime dt;
+
+dt.year = 21;
+dt.month = 8;
+dt.day = 1;
+dt.hour = 0;
+dt.min = 0;
+dt.sec = 0;
+app_datetime_set(&dt);
+
+3、获得当前时间
+datetime dt;
+
+app_datetime_get(&dt)
+
+4、获得当前时间对应的星期
+uint8_t week;
+
+app_datetime_get_week(&week);
+
+5、假设当前时间是2021.8.1/02:00:00，你要获得前一天对应的时间
+datetime dt,dt1;
+
+dt.year = 21;
+dt.month = 8;
+dt.day = 1;
+dt.hour = 2;
+dt.min = 0;
+dt.sec = 0;
+
+app_datetime_get_day_before_date(&dt,&dt1);
+dt1.year = 21;
+dt1.month = 7;
+dt1.day = 31;
+dt1.hour = 2;
+dt1.min = 0;
+dt1.sec = 0;
+
+6、假设当前时间是2021.8.1/02:00:00，你要获取该时间点前一个小时的时间
+datetime dt,dt1;
+
+dt.year = 21;
+dt.month = 8;
+dt.day = 1;
+dt.hour = 2;
+dt.min = 0;
+dt.sec = 0;
+
+app_datetime_get_pre_date_time(&dt,&dt1,60);
+dt1.year = 21;
+dt1.month = 8;
+dt1.day = 1;
+dt1.hour = 1;
+dt1.min = 0;
+dt1.sec = 0;
+
+7、app_datetime_get_behind_date_time()的使用方法同6
+*/
+
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//下面一部分是计时器
+
+//所谓计时器，是从00（hour）:00（min）:00（sec）:000（ms）处开始计时，hour是(SEC_CHR_HOUR_MAX+1)进制，
+//min、sec都是60进制，ms则是1000进制
+typedef struct
+{
+    uint16_t ms;
+	uint8_t min;
+	uint8_t hour;
+	uint8_t sec;
+}sec_chr_t;
+
+//计数器的最大小时计数值，这里是99小时
+#define SEC_CHR_HOUR_MAX        99
+
+//计时器初始化，他的目的是把ms、min、hour、sec清0
+void app_datetime_sec_chr_init(void );
+
+//获得当前的计时器时间
+void app_datetime_get_sec_chr(sec_chr_t *psec);
+
+//设置计时起始时间
+void app_datetime_set_sec_chr(sec_chr_t *psec);
+
+//同步一次RTC。这个接口的作用在于当计时器从暂停恢复到运行状态时，由于RTC计数器没有暂停，所以重启该计数器
+//时，需要先同步一次RTC，避免出现计时器计数错误
+void app_datetime_syn_rtc(void );
+
+//原理同app_datetime_synch();
+void app_datetime_sec_chr_syn(void );
+
+
+/*
+例子：
+1、初始化计时器，时、分、秒、毫秒为0
+app_datetime_sec_chr_init();
+
+2、如果暂停，调用下面接口同步RTC，以便下次启动从此刻开始计时
+app_datetime_syn_rtc();
+
+3、同步秒表
+注意，如果想计时器暂停，则不能调用这个时间同步接口。如果想让计时器从暂停到恢复，则该接口必须调用
+app_datetime_sec_chr_syn();
+
+4、获得秒表值
+app_datetime_get_sec_chr();
+
+5、设置秒表值
+sec_chr_t temp;
+temp.hour = 1;
+temp.min = 2;
+temp.sec = 3;
+temp.ms = 0;
+app_datetime_set_sec_chr(&temp);
+
+*/
+
+
+
+#endif
